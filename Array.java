f659af50b4dd9b103f1496b0333a5890b64f75a0
Samir <41807740+Samir-Belaroui@users.noreply.github.com>
2018-10-05 13:21:49 +0100

    Un peu de Swing

--- Array.java
+++ Array.java
@@ -1,19 +1,19 @@
-import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class Array {
 
 	public static void main(String[] args) {
 
-		Scanner sc =  new Scanner(System.in);
-		int tnombre [] = new int[5];
+		int tnombre[] = new int[5];
 		for (int i = 0; i < tnombre.length; i++) {
-			System.out.println("Remplisez la case numero "+(i+1));
-			tnombre [i] = sc.nextInt();
+			tnombre[i] = Integer.parseInt(JOptionPane.showInputDialog("Remplisez la case numero " + (i + 1)));
+
 		}
 		for (int j = 0; j < tnombre.length; j++) {
-			System.out.println("la case numero "+(j+1)+" contien "+(tnombre[j]));
+
+			JOptionPane.showMessageDialog(null, "la case numero " + (j + 1) + " contien " + (tnombre[j]));
 		}
-		
 
 	}
 
